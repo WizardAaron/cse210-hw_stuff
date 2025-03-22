53eabd223b7158737b01cb60d6423b96620143a9
WizardAaron <ethanlong547@gmail.com>
2025-03-22 23:02:44 +0000

    Excersise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Xml;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You guessed the number!");
     }
 }
