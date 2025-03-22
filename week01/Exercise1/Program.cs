a62aecf4994df473641269e44151e1b21571a51e
WizardAaron <ethanlong547@gmail.com>
2025-03-22 22:28:03 +0000

    Excersise1 Added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is in C#");
+
+        int number =5;
+        number = 8;
+
+        string color = "blue";
+
+        if (number > 3)
+        {
+            Console.WriteLine("number is greater than 3");
+        }
+
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
     }
 }
