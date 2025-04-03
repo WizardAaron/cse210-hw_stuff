48fba7a42b78d28553e44509c6904790ea48b6e7
WizardAaron <ethanlong547@gmail.com>
2025-04-03 01:16:13 +0000

    Team Activity: Scripture Memorizer Design complete!

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // Reference and Scripture
+        Reference reference = new Reference("1 Nephi", 3, 7);
+        Scripture scripture = new Scripture(reference, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+
+        // I have these copied here for convenience! (totally not just being lazy)
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+
+        while (true)
+        {
+            // Prompts the user as whether to continue or not
+            Console.WriteLine("Press Enter to hide 3 random words, or type 'quit' to exit: ");
+            
+            // Waits for user input
+            string input = Console.ReadLine();
+
+            // Checks if the user wants to quit
+            if (input.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Goodbye!");
+                break;
+            }
+
+            // Clears the console
+            Console.Clear();
+
+            // Hides 3 random words
+            scripture.HideRandomWords(3);
+
+            // Prints scripture with hidden words up to this point
+            Console.WriteLine(scripture.GetDisplayText());
+
+            // When all the words are hidden, ends the program
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("All words have been hidden!");
+                break;
+            }
+        }
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,86 @@
+using System;
+
+// All of the stuff going on in the background
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide && _words.Any(w => !w.IsHidden()))
+        {
+            int index = random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_reference.GetDisplayText()} {string.Join(" ", _words.Select(w => w.GetDisplayText()))}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_book} {_chapter}:{_verse}";
+    }
+}
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? new string('_', _text.Length) : _text;
+    }
+}
