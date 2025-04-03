fd5498a214de51db3f83d3899bc41e355688906b
WizardAaron <ethanlong547@gmail.com>
2025-04-03 22:46:23 +0000

    Added another verse for the Project requirements

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,8 +5,8 @@ class Program
     static void Main()
     {
         // Reference and Scripture
-        Reference reference = new Reference("1 Nephi", 3, 7);
-        Scripture scripture = new Scripture(reference, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+        Reference reference = new Reference("1 Nephi", 3, 6-7);
+        Scripture scripture = new Scripture(reference, "Therefore go, my son, and thou shalt be favored of the Lord, because thou hast not murmured; And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
 
         // I have these copied here for convenience! (totally not just being lazy)
         Console.Clear();
